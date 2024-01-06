9fc29ab48a14d5821dce37acfa1c30acf57fd3df
MirzaMinhaz <minahmed17@gmail.com>
2024-01-06 14:42:57 +0600

    commited

--- src/app/components/account-info/account-info.component.ts
+++ src/app/components/account-info/account-info.component.ts
@@ -4,7 +4,7 @@ import { MatPaginator } from '@angular/material/paginator';
 import { MatTableDataSource } from '@angular/material/table';
 import { ActivatedRoute } from '@angular/router';
 import { Router } from '@angular/router';
-import { AccountInfoService } from 'src/app/services/accountInfo/account-info.service';
+import { AccountInfoService } from 'src/app/services/accountinfo/account-info.service';
 
 @Component({
   selector: 'app-account-info',
@@ -53,6 +53,8 @@ export class AccountInfoComponent implements OnInit {
   constructor(private router: Router, private route: ActivatedRoute, private dialog: MatDialog, private accountInfoService: AccountInfoService) { }
 
   ngOnInit(): void {
+
+    debugger
 
     this.accountInfoService.getAccountType().subscribe((accType: any) => {
 
